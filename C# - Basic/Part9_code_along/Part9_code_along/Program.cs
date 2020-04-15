ef526222ab740ff58bfd32f28c5793cf75deae5f
Philippe Tome <philippe.tome@protonmail.com>
2020-04-15 11:29:02 +1000

    Part 9 exercise done

---
+++ C# - Basic/Part9_code_along/Part9_code_along/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Part9_code_along
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// 3. Demo File and FileInfo:
+            //    // File: (to run small operations it is more convenient to use static method, it can be slow for large applications)
+            //    var path = @"c:\somefile.jpg";
+            //    File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            //    File.Delete(path);
+            //    if (File.Exists(path))
+            //    {
+            //        //
+            //    }
+            //    var content = File.ReadAllText(path);
+
+            //    // FileInfo class: (if large application it is more efficient to create a FileInfo object and call the instance of that object)
+            //    var fileInfo = new FileInfo(path);
+            //    fileInfo.CopyTo("...");
+            //    fileInfo.Delete();
+            //    if (fileInfo.Exists);
+            //    {
+            //        //
+            //    }
+
+            // 4. Demo Directory and DirectoryInfo:
+                // Directory:
+                //Directory.CreateDirectory(@"c:\temp\folder1");
+
+                //var files = Directory.GetFiles(@"c:\BASE8", "*.txt", SearchOption.AllDirectories);
+                //foreach (var file in files)
+                //    Console.WriteLine(file);
+
+                //var directories = Directory.GetDirectories(@"c:\BASE8", "*.*", SearchOption.AllDirectories);
+                //foreach(var directory in directories)
+                //    Console.WriteLine(directory );
+
+                //Directory.Exists("...");
+
+                //// DirectoryInfo:
+                //var directoryInfo = new DirectoryInfo("...");
+                //directoryInfo.GetDirectories();
+
+                // Google: file info class c#. MSDN
+
+            // 5. Demo Path:
+                var path = @"C:\Projects\CSharpFundamentals\HelloWorld\HelloWorld.sln";
+
+                // Poor way of doing it:
+                var dotIndex = path.IndexOf('.');
+                var extension = path.Substring(dotIndex);
+
+                Console.WriteLine("Extension: " + Path.GetExtension(path));
+                Console.WriteLine("File Name: " + Path.GetFileName(path));
+                Console.WriteLine("File Name without extension: " + Path.GetFileNameWithoutExtension(path));
+                Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+
+        }
+    }
+}
---
+++ C# - Basic/Part9_exercise/Part9_exercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Part9_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - Write a program that reads a text file and displays the number of words.
+            //var path = @"C:\Users\ptome\MEGA\Shared Emma - Philippe\Philippe\Study\Developer\C-Sharp\C# - Basic\test.txt";
+
+            //var content = File.ReadAllText(path).Split(' ');
+            
+            //Console.WriteLine(content.Length);
+
+            //2 - Write a program that reads a text file and displays the longest word in the file.
+            var path = @"C:\Users\ptome\MEGA\Shared Emma - Philippe\Philippe\Study\Developer\C-Sharp\C# - Basic\test.txt";
+
+            var content = File.ReadAllText(path).Split(' ');
+
+            // Assume first word is the longest:
+            var longestWord = content[0];
+            foreach (var word in content)
+            {
+                if (word.Length > longestWord.Length)
+                    longestWord = word;
+            }
+
+            Console.WriteLine(longestWord);
+        }
+    }
+}
