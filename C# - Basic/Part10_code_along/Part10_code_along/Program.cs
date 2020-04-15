0d69fa5599b5595fe5b21b9d9024e581aa4ff635
Philippe Tome <philippe.tome@protonmail.com>
2020-04-15 15:38:35 +1000

    Part 10 upload

---
+++ C# - Basic/Part10_code_along/Part10_code_along/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part10_code_along
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> {1, 2, 3, 4, 5, 6};
+            var smallests = GetSmallests(numbers, 3);
+
+            foreach (var number in smallests)
+                Console.WriteLine(number);
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the nubmer of elements in the list.");
+
+            var buffer = new List<int>(list);
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(buffer);
+                smallests.Add(min);
+                buffer.Remove(min);
+            }
+
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            // Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
+
+    }
+}
